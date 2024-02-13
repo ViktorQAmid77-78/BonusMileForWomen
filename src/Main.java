0089514d1a5020cf901b14e41a46f712a64ba571
Viktor Alekseevich <com.brick.ozon@gmail.com>
2024-02-13 14:58:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 23120;
+        boolean isWomen = true;
+        int bonus = 20;
+        int mile = score / bonus;
+        System.out.println("Итоговые бонусные мили:" + mile);
+    }
+
+}
